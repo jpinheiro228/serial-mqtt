4047a484d0145aaec7b00def922bfbb751f811bb
Joao Pinheiro <joao.jfnp@gmail.com>
2022-06-21 22:26:30 +0200

    mid-commit

--- main.py
+++ main.py
@@ -4,14 +4,32 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from serial import Serial
 import datetime
+import paho.mqtt.publish as publish
+import json
+
+with open("config.json","r") as f:
+    cfg = json.load(f)
+CLIENT_ID=cfg["id"]
+BROKER = cfg["broker"]
+TOPIC = cfg["topics"][0]
+CREDS = {"username": cfg["username"], "password": cfg["password"]}
+
+def publishmqtt(m, t):
+    publish.single(topic=t, payload=m, hostname=BROKER,
+                   client_id=CLIENT_ID, retain=True, auth=CREDS)
+
+
+def readserial(s):
+    try:
+        print(datetime.datetime.now(), end="\t")
+        return s.readline().decode().splitlines()[0]
+    except:
+        s.close()
+
 
 if __name__ == '__main__':
-    s = Serial(port="/dev/ttyUSB0", baudrate=115200)
-    while s.isOpen():
-        try:
-            print(datetime.datetime.now(), end="\t")
-            print(s.readline().decode().splitlines()[0])
-        except:
-            s.close()
-            break
+    # s = Serial(port="/dev/ttyUSB0", baudrate=115200)
+    # while s.isOpen():
+    #     readserial(s)
+    publishmqtt("hello!", TOPIC)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
